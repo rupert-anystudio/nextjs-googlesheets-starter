7c586700fc53ea7032be948094b09cc9bec40bba
Rupert Adlmaier <rupert@any.studio>
2021-09-30 12:31:23 +0200

    pulled clients list from sheets into index page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,29 @@
+import { getClients } from '../lib/api'
 
-export default function Home() {
+export default function Home({ clients }) {
   return (
     <>
       <h1>Home</h1>
+      <h2>Clients:</h2>
+      <ul>
+        {clients.map(client => {
+          return (
+            <li key={client.title}>
+              {client.title}
+            </li>
+          )
+        })}
+      </ul>
     </>
   )
+}
+
+export async function getStaticProps(context) {
+  const clients = await getClients()
+  return {
+    props: {
+      clients,
+    },
+    revalidate: 30,
+  }
 }
